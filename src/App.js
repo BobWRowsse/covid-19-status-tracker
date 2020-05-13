14647914d232d792eb16f805176a9b7d71927ccf
BobWRowsse <bobrowsse.walakira@hyperisland.se>
2020-05-13 20:33:45 +0200

    fetch API data / visualize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React from 'react';
 
-import { Cards, Chart, CountryPicker } from "./components";
-import styles from "./App.module.css";
-import { fetchData } from "./api";
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
   state = {
     data: {},
+    country: '',
   };
 
   async componentDidMount() {
-    const data = await fetchData();
+    const fetchedData = await fetchData();
 
-    this.setState({ data: data });
+    this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
-        <CountryPicker />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     );
