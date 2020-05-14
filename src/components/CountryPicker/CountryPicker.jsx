80315a08d5cc2aedbbf4c6a5caf5fa342a932582
BobWRowsse <bobrowsse.walakira@hyperisland.se>
2020-05-14 09:59:35 +0200

    add image and finalise app

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NativeSelect, FormControl, StylesProvider } from '@material-ui/core';
+import { NativeSelect, FormControl } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 
 import { fetchCountries } from '../../api';
@@ -17,7 +17,7 @@ const CountryPicker = ({ handleCountryChange }) => {
   return (
     <FormControl className={styles.formControl}>
       <NativeSelect defaultValue='' onChange={(e) => handleCountryChange(e.target.value)}>
-        <option value='global'>Global</option>
+        <option value=''>Global</option>
         {fetchedCountries.map((country, i) => (
           <option key={i} value={country}>
             {country}{' '}
